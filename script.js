474e809a4e0f189b3a228f2d5e93fbc7ffbe94fb
Ezequiel-Silva <70416107+Ezequiel-Silva@users.noreply.github.com>
2022-11-14 11:26:58 -0300

    Add files via upload
    Adding the Javascript file in the 01_SentencaDeCodigo branch

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Letras > Palavras > Frase > Parágrafo >
+Páginas > Secções > Capítulos > LIVRO */
+
+// Sentença = Frases
+// Bloco = Parágrafos
+
+// Exibir msg no console/terminal
+console.log("Bom dia!"); // Sentença de código
+console.log("Boa tarde!"); // Sentença de código
+console.log("Boa noite!"); // Sentença de código
+
